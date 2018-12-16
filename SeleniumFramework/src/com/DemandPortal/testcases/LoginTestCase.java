e32019c7a56f20755eec1cc76baf7ab33c96786e
USER <USER@192.168.0.40>
2018-12-16 21:46:47 +0530

    Updated Login test case

--- SeleniumFramework/src/com/DemandPortal/testcases/LoginTestCase.java
+++ SeleniumFramework/src/com/DemandPortal/testcases/LoginTestCase.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
